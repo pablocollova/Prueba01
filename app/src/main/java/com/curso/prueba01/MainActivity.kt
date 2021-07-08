769863504857ad36195ea9c81882089d2e55041d
Pablo Collova <pablocollova@gmail.com>
2021-07-08 13:58:12 -0300

    prueba 06 probando la otra compu

--- app/src/main/java/com/curso/prueba01/MainActivity.kt
+++ app/src/main/java/com/curso/prueba01/MainActivity.kt
@@ -7,9 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        rfrfrfrf
-        frf
-        fgfgfg
-ffffff
+bla bla bals
     }
 }
