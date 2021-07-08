09f7ce84a11b7175e523a424ffe3effb61dfb5a8
Pablo Collova <pablocollova@gmail.com>
2021-07-08 15:21:24 -0300

    moureDev intermedio 01

--- app/src/main/java/com/curso/prueba01/MainActivity.kt
+++ app/src/main/java/com/curso/prueba01/MainActivity.kt
@@ -7,6 +7,45 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-bla bla bals
+
+
+        // enum clases
+        enumClasses()
+
+    }
+
+    // lección 1 kotlin intermedio: Enum Classes
+    enum class Direction(val dir : Int){
+        NORTH(dir=1), SOUTH(dir=-1), EAST(dir=1), WEST(dir=-1);
+        fun description():String{
+            return when(this){
+                NORTH-> "la dirección es NORTE"
+                SOUTH-> "la dirección es SUR"
+                EAST->"la dirección es ESTE"
+                WEST->"la dirección es OESTE"
+            }
+        }
+    }
+
+    private fun enumClasses(){
+        //Asignación de valores
+        var userDirection: Direction?=null
+        println("Dirección: $userDirection")
+
+        userDirection=Direction.NORTH
+        println("Dirección: $userDirection")
+
+        userDirection=Direction.WEST
+        println("Dirección: $userDirection")
+
+        //propiedades por defecto
+        println("propiedad name: ${userDirection.name}")
+        println("propiedad ordinal: ${userDirection.ordinal}")
+
+        //funciones
+        println(userDirection.description())
+
+        //Inicialización
+        println(userDirection.dir)
     }
 }
